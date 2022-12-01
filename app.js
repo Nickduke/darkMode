2f2d84f8f985c571d169d888221b4c37f07610ed
Nick <ntoukas.nick@gmail.com>
2022-12-01 12:15:59 -0500

    render items onto page

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+import articles from './data.js';
+
 const btn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
 
 btn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark-mode');
 });
+
+const articlesData = articles
+  .map(({ title, date, length, snippet }) => {
+    return `<article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${date}</span>
+            <span>${length}</span>
+          </div>
+          <p>
+           ${snippet}
+          </p>
+        </article>`;
+  })
+  .join('');
+
+articlesContainer.innerHTML = articlesData;
--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const articles = [
   {
     id: 1,
-    title: 'the WET Codbase',
+    title: 'the WET Codebase',
     date: new Date(2020, 9, 4),
     length: 11,
     snippet: `I'm baby ramps kombucha gluten-free ennui swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
@@ -28,3 +28,5 @@ const articles = [
     snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
   },
 ];
+
+export default articles;
